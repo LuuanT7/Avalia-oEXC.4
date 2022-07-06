7e2d036e50cac62ddb120190bfe9c4947e072349
luuan <luuan@DESKTOP-J5ICT1R>
2022-07-05 23:51:30 -0300

    Adicionar arquivos de projeto.

---
+++ AvaliaçãoEXC.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliaçãoEXC._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 4.");
+            
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
